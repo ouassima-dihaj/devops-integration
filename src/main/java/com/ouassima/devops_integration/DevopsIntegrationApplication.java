1ac95c7e45e47d486f01958b5a847bcbc1563326
Ouassima Dihaj <ouassimadihaj1@gmail.com>
2025-01-01 17:47:51 +0100

    add end point and change port to 8081

--- src/main/java/com/ouassima/devops_integration/DevopsIntegrationApplication.java
+++ src/main/java/com/ouassima/devops_integration/DevopsIntegrationApplication.java
@@ -2,10 +2,16 @@ package com.ouassima.devops_integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevopsIntegrationApplication {
-
+	@GetMapping("/hey")
+	public void sayHey(){
+		System.out.println("Hello world !");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsIntegrationApplication.class, args);
 	}
