9ac2bb70b5e545ab0b7ef9b1803b56577ba7dfd9
Ouassima Dihaj <ouassimadihaj1@gmail.com>
2025-01-01 16:50:50 +0100

    first commit

---
+++ src/test/java/com/ouassima/devops_integration/DevopsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouassima.devops_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
